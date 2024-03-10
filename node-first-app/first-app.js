6f2659888b00dce3515940a2f9d8590c5faa7c1f
dunky-star <dunkygeoffrey39@gmail.com>
2024-03-10 06:24:46 -0500

    promise

--- node-first-app/first-app.js
+++ node-first-app/first-app.js
@@ -8,12 +8,54 @@
 // );
 
 const userName = 'Geoffrey Duncan Opiyo';
-let age = 30;
+let myAge = 30;
 const hasHobbies = true;
-age = 33;
+myAge = 33;
 
-const summarizeUser = (userName, userAge, userHobbies) => {
-  return `Name is ${userName}. Age is ${userAge} and user has hobbies: ${userHobbies}`;
+// Arrow Function
+const summarizeUser = (userName, userAge, userHobbies) =>
+  `My name is ${userName}. Age is ${userAge} and user has hobbies: ${userHobbies}`;
+
+console.log(summarizeUser(userName, myAge, hasHobbies));
+
+// JavaScript Object
+const person = {
+  name: 'Geoffrey',
+  age: 33,
+  greet() {
+    console.log('Hi there, I am ' + this.age + ' again');
+  },
 };
 
-console.log(summarizeUser(name, age, hasHobbies));
+person.greet();
+
+// Destructuring
+
+const { name, age } = person;
+console.log(name, age);
+
+// Async && Promise
+
+const fetchData = () => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Done!');
+    }, 1500);
+  });
+  return promise;
+};
+
+setTimeout(() => {
+  console.log('Timer is done!');
+  fetchData()
+    .then(text => {
+      console.log(text);
+      return fetchData();
+    })
+    .then(text2 => {
+      console.log(text2);
+    });
+}, 2000);
+
+console.log('Hello!');
+console.log('Hi!');
