1dee724a376b83ead09a7c46c7403cc8636bd22c
dunky-star <dunkygeoffrey39@gmail.com>
2024-03-09 22:48:17 -0600

    Understanding arrow function

--- node-first-app/first-app.js
+++ node-first-app/first-app.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 // const fs = require("fs");
 
@@ -6,3 +6,14 @@
 //   "hello.txt",
 //   "I am a from Kampala Uganda studying Computer Science from the US"
 // );
+
+const userName = 'Geoffrey Duncan Opiyo';
+let age = 30;
+const hasHobbies = true;
+age = 33;
+
+const summarizeUser = (userName, userAge, userHobbies) => {
+  return `Name is ${userName}. Age is ${userAge} and user has hobbies: ${userHobbies}`;
+};
+
+console.log(summarizeUser(name, age, hasHobbies));
